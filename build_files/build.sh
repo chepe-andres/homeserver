32e926475914bd4b94360656bff07da844cde9cf
chepe-andres <andresishmael@protonmail.com>
2026-01-26 17:47:59 -0500

    home server bootc

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,56 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -ouex pipefail
+set -xeuo pipefail
 
-### Install packages
+dnf update -y
+dnf install -y epel-release
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf install -y "https://zfsonlinux.org/epel/zfs-release-3-0$(rpm --eval "%{dist}").noarch.rpm"
+dnf install -y kernel-devel
+dnf install -y zfs
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | grep -v kabi | tail -n 1)"
+ZFS_VERSION="$(find /usr/src -maxdepth 1 -iname "zfs*" -exec basename '{}' ';' | cut -f2 -d-)"
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dkms install -m zfs -v "${ZFS_VERSION}" -k "${KERNEL_VERSION}"
+cat /var/lib/dkms/*/*/build/make.log || :
 
-#### Example for enabling a System Unit File
+dnf config-manager --add-repo "https://negativo17.org/repos/epel-nvidia-580.repo"
+dnf config-manager --set-disabled "epel-nvidia-580"
+dnf config-manager --save --setopt=epel-nvidia-580.priority=90
+dnf install -y --enablerepo="epel-nvidia-580" akmod-nvidia
 
-systemctl enable podman.socket
+dnf -y install gcc-c++
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
+cat /var/cache/akmods/nvidia/*.failed.log || true
+
+dnf config-manager --add-repo "https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo"
+dnf config-manager --set-disabled "epel-nvidia-580"
+dnf install -y --enablerepo="nvidia-container-toolkit" nvidia-container-toolkit
+
+dnf install -y --enablerepo="epel-nvidia-580" --enablerepo="nvidia-container-toolkit" \
+    "libnvidia-fbc" \
+    "libnvidia-ml" \
+    "nvidia-driver" \
+    "nvidia-driver-cuda" 
+
+tee /usr/lib/modprobe.d/00-nouveau-blacklist.conf <<'EOF'
+blacklist nouveau
+options nouveau modeset=0
+EOF
+
+echo zfs >/usr/lib/modules-load.d/zfs.conf
+echo nvidia >/usr/lib/modules-load.d/nvidia.conf
+
+
+tee /usr/lib/bootc/kargs.d/00-nvidia.toml <<'EOF'
+kargs = ["rd.driver.blacklist=nouveau", "modprobe.blacklist=nouveau", "nvidia-drm.modeset=1"]
+EOF
+
+# we must force driver load to fix black screen on boot for nvidia desktops
+sed -i 's@omit_drivers@force_drivers@g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
+
+# as we need forced load, also mustpre-load intel/amd iGPU else chromium web browsers fail to use hardware acceleration
+sed -i 's@ nvidia @ i915 amdgpu nvidia @g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
+
+dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/lib/modules/$KERNEL_VERSION/initramfs.img"
